2acea7cf543d2473cfc06a6d91992ef357b17b91
Brian Kimble <brian@stickbuilt.us>
2025-10-23 15:37:33 +0000

    Okay one thing I want to change about the layout is I want the padding o

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,12 @@ export default {
   theme: {
     container: {
       center: true,
-      padding: '2rem',
+      padding: {
+        DEFAULT: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
       screens: {
         '2xl': '1400px',
       },
