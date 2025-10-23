ec37145e507f24f08457bdbfe94b107fe3d22635
Brian Kimble <brian@stickbuilt.us>
2025-10-23 15:07:10 +0000

    Change the heading font (h1-h6) to be Plus Jakarta Sans and make the bod

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Alegreya:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Plus+Jakarta+Sans:wght@700;800&display=swap"
           rel="stylesheet"
         />
       </head>
