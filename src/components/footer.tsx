e2a35239c79cfff22e361b9df93d4d3748c81a6e
Brian Kimble <brian@stickbuilt.us>
2025-10-23 16:04:24 +0000

    Actually make the footer background white and put the logo/site name inf

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,11 +3,11 @@ import { Logo } from "./logo";
 
 export function Footer() {
   return (
-    <footer className="border-t bg-muted">
+    <footer className="border-t bg-background">
       <div className="container py-8">
-        <div className="flex flex-col items-center gap-4">
+        <div className="flex items-center justify-between">
           <Logo />
-          <p className="text-center text-sm leading-loose text-muted-foreground">
+          <p className="text-sm text-muted-foreground">
             © {new Date().getFullYear()} {siteConfig.name}. All Rights Reserved.
           </p>
         </div>
