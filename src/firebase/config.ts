6f55dc91e33a759ad0984680dce89fb9e030a545
Brian Kimble <brian@stickbuilt.us>
2025-10-27 16:26:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6079651880-f27a0",
+  "appId": "1:493696825089:web:3d2b307a5898a0b1b94d00",
+  "apiKey": "AIzaSyAYMZMgZByWw0dAPSLCXsmHrZYtGGIMlSA",
+  "authDomain": "studio-6079651880-f27a0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "493696825089"
+};
